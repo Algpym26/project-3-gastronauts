e3923598869d1d1c561855ecc2388745cd57a458
Algpym26 <110873247+Algpym26@users.noreply.github.com>
2023-01-27 23:38:57 -0500

    header, footer, navbar

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import{BsFillTelephoneInboundFill} from"react-icons/bs"
+import{MdEmail} from"react-icons/md"
+const Footer = () => {
+  return (
+    <div>
+    <h2>Contact Us</h2>  
+    <BsFillTelephoneInboundFill/>
+    <MdEmail/>
+    </div>
+  )
+}
+
+export default Footer
