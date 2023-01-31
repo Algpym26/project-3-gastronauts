2c1276eae6348be78dd33eebc8bd76712e51502e
Brayden <brayburl224@gmail.com>
2023-01-30 20:26:08 -0500

    did a few minor tweaks and am trying to adjust time selection so that only every 15 minutes shows

--- HTML Drafts/assets/js/reservation.js
+++ HTML Drafts/assets/js/reservation.js
@@ -1,6 +1,9 @@
 var currentDateTime = new Date();
+
 var year = currentDateTime.getFullYear();
+
 var month = (currentDateTime.getMonth() + 1);
+
 var date = (currentDateTime.getDate() + 1);
 
 if(date < 10) {
@@ -11,11 +14,16 @@ if(month < 10) {
 }
 
 var dateTomorrow = year + "-" + month + "-" + date;
+
 var reservationDate = document.querySelector("#reservation-date");
+
 var reservationTime = document.querySelector("#reservation-time");
 
 reservationDate.setAttribute("min", dateTomorrow);
 
 reservationTime.onchange = function () {
     checkoutElem.setAttribute("min", this.value);
+    if(this.value.min < 15) {
+      min = 0;
+    }
 }
