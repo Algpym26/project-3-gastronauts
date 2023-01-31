df13f186f546dc965e288eae896520953e26fe4c
Brayden <brayburl224@gmail.com>
2023-01-30 21:25:10 -0500

    drafted out reservationForm component, moved images and js files into assets folder, moved two css files into css folder, and deleted temp folder that I used to store all of this in prior to push

--- client/src/components/reservationForm.js
+++ client/src/components/reservationForm.js
@@ -6,20 +6,86 @@ const reservationForm = () => {
 
     const phoneChecker = new RegExp("(\d{3})-?\s?(\d{3})-?\s?(\d{4})");
 
+    const textBoxMessage = "Dietary restrictions, handicap accessibility, special occasion, or something not listed here?";
+
+    var currentDateTime = new Date();
+
+var year = currentDateTime.getFullYear();
+
+var month = (currentDateTime.getMonth() + 1);
+
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+
+var reservationDate = document.querySelector("#reservation-date");
+
+var reservationTime = document.querySelector("#reservation-time");
+
+reservationDate.setAttribute("min", dateTomorrow);
+
+reservationTime.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+    if(this.value.min < 15) {
+      min = 0;
+    }
+}
+
     return (
         <div>
             <form action = "reservation.php" method = "post">
                 <div className = "elem-group">
                     <label for = "name">Your Preferred Name</label>
-                    <input type="text" id="name" name="customer_name" placeholder="Mariely Brite" pattern = {userChecker} required/>
+                    <input type = "text" id = "name" name = "customer_name" placeholder = "Mariely Brite" pattern = {userChecker} required/>
                 </div>
-                <div class="elem-group">
-              <label for="email">Your E-mail</label>
-              <input type="email" id="email" name="visitor_email" placeholder="marbrite10@email.com" required/>
-              <div class="elem-group">
-              <label for="phone">Your Phone Number (We text updates)</label>
-              <input type = "tel" id = "phone" name = "customer_phone" placeholder = "498-348-3872" pattern= {phoneChecker} required />
+                <div class = "elem-group">
+              <label for = "email">Your E-mail</label>
+              <input type = "email" id = "email" name = "visitor_email" placeholder = "marbrite10@email.com" required/>
+              <div class = "elem-group">
+              <label for = "phone">Your Phone Number (We text updates)</label>
+              <input type = "tel" id = "phone" name = "customer_phone" placeholder = "498-348-3872" pattern = {phoneChecker} required />
             </div>
+            <hr/>
+            <div class = "elem-group inlined">
+              <label for = "adult">Adults</label>
+              <input type = "number" id = "adult" name = "total_adults" placeholder = "2" min = "1" required />
+            </div>
+            <div class = "elem-group inlined">
+              <label for = "child">Children</label>
+              <input type = "number" id = "child" name = "total_children" placeholder = "2" min = "0" required />
+            </div>
+            <div class = "elem-group inlined">
+              <label for = "reservation-date">Reservation Date</label>
+              <input type ="date" id = "reservation-date" name = "reservation_date" required />
+            </div>
+            <div class = "elem-group inlined">
+              <label for = "reservation-time">Reservation Time</label>
+              <input type = "time" id = "reservation-time" name = "reservation_time" step = "900" required />
+            </div>
+            <div class = "elem-group">
+              <label for = "seating-selection">Seating Preference</label>
+              <select id = "seating-selection" name = "seating_preference" required>
+                  <option value = "">Choose a seating option or to-go</option>
+                  <option value = "booth-seating">Booth</option>
+                  <option value = "bar-seating">Bar</option>
+                  <option value = "table-seating">Table</option>
+                  <option value = "patio-seating">Patio</option>
+                  <option value = "no-seating">No seating / To-Go Order</option>
+              </select>
+            </div>
+            <hr/>
+            <div class = "elem-group">
+              <label for = "message">Is there anything else we should know about?</label>
+              <textarea id = "message" name = "visitor_message" placeholder = {textBoxMessage} required></textarea>
+            </div>
+            <button type = "submit">Reservation Takeoff!</button>
             </div>
             </form>
         </div>
