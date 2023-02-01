313b0c1fbf365375aba227ee35b6edb69de9120c
Brayden <brayburl224@gmail.com>
2023-02-01 18:54:19 -0500

    importing the necessary components and tools for App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import RestaurantCards from './components/RestaurantCards';
+import ReservationForm from './components/ReservationForm';
+import Footer from './components/Footer';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client = {client}>
+    <Router>
+    
+    </Router>
+    </ApolloProvider>
   );
 }
 
