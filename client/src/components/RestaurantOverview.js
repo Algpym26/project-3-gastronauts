a65792485fe8793a7e0621931ed63687cef5dcd8
bmigo <berniecmigo@gmail.com>
2023-02-01 19:56:35 -0500

    finished react translation

--- client/src/components/RestaurantOverview.js
+++ client/src/components/RestaurantOverview.js
@@ -5,13 +5,26 @@ const RestaurantOverview = () => {
         <div className='card'>
             <section className='card container'>
                 <h1>Restaurant Title</h1>
-                <img src="../../asset/restaurant01.jpg"></img>
+                <img src="./asset/images/restaurant01.jpg"></img>
                 <p>This is a description of the restaurant</p>
                 <p>Address</p>
                 <h2>Our Menu</h2>
                 <div className='menu'> 
                     <p>BURGER - This is a well-written description of the menu item. It is very delicious and very nutritious. $15</p>
                 </div>
+                <h2>Hours of Operation</h2>
+                <p>
+                    11AM - 9PM Monday through Thursday {'\n'}
+                    11AM - 12AM Friday through Saturday {'\n'}
+                    CLOSED Sunday
+                </p>
+                <h2>Tags</h2>
+                <div>
+                    <span class="label other">Dog Friendly</span>
+                    <span class="label other">Gluten Free</span>
+                </div>
+                {'\n'}
+                <button className='resBtn'>Make A Reservation</button>
             </section>
         </div>
     )
