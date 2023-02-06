cd44fabc77efa979e6a97e5791ccb545ef283868
Brayden <brayburl224@gmail.com>
2023-02-06 10:51:32 -0500

    fixed class to className in ReservationForm as well as placed date/time inside a function and called it --> still receiving an error regarding the setAttribute part

--- client/src/components/reservationForm.js
+++ client/src/components/reservationForm.js
@@ -10,7 +10,9 @@ const ReservationForm = () => {
 
     const textBoxMessage = "Dietary restrictions, handicap accessibility, special occasion, or something not listed here?";
 
-    var currentDateTime = new Date();
+    function reservationDate(){
+
+var currentDateTime = new Date();
 
 var year = currentDateTime.getFullYear();
 
@@ -35,7 +37,9 @@ reservationDate.setAttribute("min", saveDate);
 
 reservationTime.onchange = function () {
     reservationDate.setAttribute("min", this.value);
-}
+}}
+
+reservationDate();
 
     return (
         <div>
@@ -45,31 +49,31 @@ reservationTime.onchange = function () {
                     <label for = "name">Your Preferred Name</label>
                     <input type = "text" id = "name" name = "customer_name" placeholder = "Mariely Brite" pattern = {userChecker} required/>
                 </div>
-                <div class = "elem-group">
+                <div className = "elem-group">
               <label for = "email">Your E-mail</label>
               <input type = "email" id = "email" name = "visitor_email" placeholder = "marbrite10@email.com" required/>
-              <div class = "elem-group">
+              <div className = "elem-group">
               <label for = "phone">Your Phone Number (We text updates)</label>
               <input type = "tel" id = "phone" name = "customer_phone" placeholder = "498-348-3872" pattern = {phoneChecker} required />
             </div>
             <hr/>
-            <div class = "elem-group inlined">
+            <div className = "elem-group inlined">
               <label for = "adult">Adults</label>
               <input type = "number" id = "adult" name = "total_adults" placeholder = "2" min = "1" required />
             </div>
-            <div class = "elem-group inlined">
+            <div className = "elem-group inlined">
               <label for = "child">Children</label>
               <input type = "number" id = "child" name = "total_children" placeholder = "2" min = "0" required />
             </div>
-            <div class = "elem-group inlined">
+            <div className = "elem-group inlined">
               <label for = "reservation-date">Reservation Date</label>
               <input type ="date" id = "reservation-date" name = "reservation-date" required />
             </div>
-            <div class = "elem-group inlined">
+            <div className = "elem-group inlined">
               <label for = "reservation-time">Reservation Time</label>
               <input type = "time" id = "reservation-time" name = "reservation-time" step = "900" required />
             </div>
-            <div class = "elem-group">
+            <div className = "elem-group">
               <label for = "seating-selection">Seating Preference</label>
               <select id = "seating-selection" name = "seating_preference" required>
                   <option value = "">Choose a seating option or to-go</option>
@@ -81,7 +85,7 @@ reservationTime.onchange = function () {
               </select>
             </div>
             <hr/>
-            <div class = "elem-group">
+            <div className = "elem-group">
               <label for = "message">Is there anything else we should know about?</label>
               <textarea id = "message" name = "visitor_message" placeholder = {textBoxMessage} required></textarea>
             </div>
