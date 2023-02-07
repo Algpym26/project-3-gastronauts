4b8762c73f6275d9ab6ca9e53f9ab5ad7d5faa48
Algpym26 <110873247+Algpym26@users.noreply.github.com>
2023-02-06 20:03:11 -0500

    UD

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,13 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
-
+ 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
         return awaitUser.findOne({ _id: context.user._id });
-      }
+      } 
       throw new AuthenticationError("We cannot find that user");
     },
   },
