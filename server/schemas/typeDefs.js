ebbbbd99534e823779beeacf92486972baec57b3
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-02-06 19:17:09 -0500

    able to run server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,12 +10,25 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(username: String! email: String!, password: String!): Auth
-    login(email: String!, email: String! password:String!): Auth
-    makeReservation(resInfo: ResInput! ): User
+    login( email: String! password:String!): Auth
+    makeReservation(resInfo:ResInfo! ): User
     cancelReservation(restaurant_id: ID!): User
 
   }
 
+    input ResInfo {
+
+      restaurant_id: ID!
+    name: String!
+    location: String!
+    description: String!
+    image: String!
+    hours: String!
+    menu: [String]
+    tags: [String]
+
+    }
+  
   type User {
     _id: ID!
     username: String!
